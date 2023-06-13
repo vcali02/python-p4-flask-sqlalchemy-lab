5716178e089e62792c605ee13790c898313f095f
valeria caliguire <caliguireval@gmail.com>
2023-06-13 11:52:33 -0600

    completed with support, understand better

--- server/app.py
+++ server/app.py
@@ -13,21 +13,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+#Each view should display all attributes as line items (ul). 
+#If there is a one-to-many relationship, 
+#each of the many should have its own line item.
+
+
+
+
 @app.route('/')
 def home():
     return '<h1>Zoo app</h1>'
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    #if the Animal.id is equal to the ID IN THE PARAMETER....
+    animal = Animal.query.filter(Animal.id == id).first()
+    #declare variable for what will be the BODY OF YOUR RESPONSE
+    #declare it to an "interpolable" empty string
+    response_body = f''
+    #ADD the interpolated string to the empty string from above
+    response_body += f'<ul>ID: {animal.id}</ul>'
+    response_body += f'<ul>Name: {animal.name}</ul>'
+    response_body += f'<ul>Species: {animal.species}</ul>'
+    response_body += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
+    response_body += f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
+    #use make_response method
+    return make_response(response_body)
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    response_body = f''
+    response_body += f'<ul>ID: {zookeeper.id}</ul>'
+    response_body += f'<ul>Name: {zookeeper.name}</ul>'
+    response_body += f'<ul>Birthday: {zookeeper.birthday}</ul>'
+
+    #WHY A LOOP?
+    #for every animal in the zookeeper table under the animals attribute
+    for animal in zookeeper.animals:
+    #grab the empty interpolable string that will makeup the response body
+    #and add the interpolated string
+    #that interpolated string will be grabbing the animals attribute, 
+    #specifically the name associated with it
+        response_body += f'<ul>Animals: {animal.name}</ul>'
+
+    return make_response(response_body)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    response_body = f''
+    response_body += f'<ul>ID: {enclosure.id}</ul>'
+    response_body += f'<ul>Environment: {enclosure.environment}</ul>'
+    response_body += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
+
+    for animal in enclosure.animals:
+        response_body += f'<ul>Animals: {animal.name}</ul>'
+    return make_response(response_body)
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,17 +7,59 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+#The Zookeeper model should contain:
+# a name, a birthday, and a list of animals that they take care of.
 class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
 
+    name = db.Column(db.String)
+    birthday = db.Column(db.String)
+    #animals = db.Column(db.String)
+
+    #RELATIONSHIP
+    #Zookeeper class animalse attribute is accessed through the Animal class zookeeper attribute
+    animals = db.relationship('Animal', back_populates= 'zookeeper')
+
+
+
+#The Enclosure model should contain:
+# an environment (grass, sand, or water), an open_to_visitors boolean, and a list of animals
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
 
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+    #animals = db.Column(db.String)
+
+    #RELATIONSHIP
+    #Enclosure class animals attribute is accessed through the Animal class enclosure attribute
+    animals = db.relationship('Animal', back_populates= 'enclosure')
+
+
+
+#The Animal model should contain:
+# a name, a species, a zookeeper, and an enclosure.
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    #zookeeper = db.Column(db.String)
+    #enclosure = db.Column(db.String)
+
+    #FOREIGN KEY
+    # variable name   make a column (contains int)  make foreign key and pass param table_name.id
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+
+    #RELATIONSHIP
+    #Animal class zookeeper attribute is accessed through the Zookeeper class animals_in_care attribute
+    zookeeper = db.relationship('Zookeeper', back_populates= 'animals')
+    #Animal class enclosure attribute is accessed through the Enclosure class animals_list attribute
+    enclosure = db.relationship('Enclosure', back_populates= 'animals')
